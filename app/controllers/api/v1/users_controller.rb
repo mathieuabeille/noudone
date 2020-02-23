7a2a0d605bda01a63b52595de92a172ffbe9e6f4
Mathieu Abeille <mathieu.abeille@gmail.com>
2020-02-23 15:13:10 +0100

    back

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,42 +5,43 @@ module Api
 				users = User.order('created_at');
 				render json: {status: 'SUCCESS', message:'Loaded users', data:users},status: :ok
 			end
-def show
-			user = User.find(params[:id])
-        render json: { status: 'SUCCESS', message:'Loaded users', data:user},status: :ok
-	end
+      def show
+       user = User.find(params[:id])
+       render json: { status: 'SUCCESS', message:'Loaded users', data:user},status: :ok
+     end
 
 
-	      def create
-        user = User.new(user_params)
+     def create
+      user = User.new(user_params)
 
-        if user.save
-          render json: {status: 'SUCCESS', message:'Saved user', data:user},status: :ok
-        else
-          render json: {status: 'ERROR', message:'User created', data:user.errors},status: :unprocessable_entity
-        end
+      if user.save
+        render json: {status: 'SUCCESS', message:'Saved user', data:user},status: :ok
+      else
+        render json: {status: 'ERROR', message:'User created', data:user.errors},status: :unprocessable_entity
       end
+    end
 
-            def destroy
-        user = User.find(params[:id])
-        user.destroy
-        render json: {status: 'SUCCESS', message:'Deleted article', data:user},status: :ok
+    def destroy
+      user = User.find(params[:id])
+      user.destroy
+      render json: {status: 'SUCCESS', message:'Deleted article', data:user},status: :ok
+    end
+
+    def update
+      user = User.find(params[:id])
+      if user.update_attributes(user_params)
+        render json: {status: 'SUCCESS', message:'Updated article', data:user},status: :ok
+      else
+        render json: {status: 'ERROR', message:'Article not updated', data:user.errors},status: :unprocessable_entity
       end
+    end
 
-      def update
-        user = User.find(params[:id])
-        if user.update_attributes(user_params)
-          render json: {status: 'SUCCESS', message:'Updated article', data:user},status: :ok
-        else
-          render json: {status: 'ERROR', message:'Article not updated', data:user.errors},status: :unprocessable_entity
-        end
-      end
+    private
 
-       private
+    def user_params
+     params.permit(:firstName, :lastName, :nickName, :email, :password, :avatarUrl)
+   end
+ end
+end
+end
 
-      def user_params
- params.permit(:firstName, :lastName, :nickName, :email, :password, :avatarUrl)     
-end
-end
-end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,28 @@
 Rails.application.routes.draw do
-  
 
-    resources :flows do 
-      resources :ratings
-      resources :tasks do
-          resources :videos
-          resources :checklists
-          resources :forms
-          resources :images
-          resources :batches do
-            resources :interactions
+  namespace 'api' do
+    namespace 'v1' do
+      resources :flows , defaults: { format: :json }, only: [:index] do
+          resources :ratings , only: [:index]
+          resources :tasks , only: [:index] do
           end
+        end
+      resources :users , defaults: { format: :json } do
+        resources :flows do
+          resources :ratings
+          resources :tasks do
+            resources :videos
+            resources :checklists
+            resources :forms
+            resources :images
+            resources :batches do
+              resources :interactions
+            end
+          end
+        end
       end
     end
-  
-  namespace 'api' do
-    namespace 'v1' do    
-resources :users , defaults: { format: :json } 
   end
-end
   root to: 'users#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
