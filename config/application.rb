c791d65d66a279eea7dec12140ac020dd93fbd69
Mathieu Abeille <mathieu.abeille@gmail.com>
2020-02-24 10:30:37 +0100

    fg

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Noudone
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.web_console.development_only = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
